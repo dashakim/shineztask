1cdbc37e2a075b385470a9289da10882e89fc7d1
Daria Tabala <dasha.tabala@gmail.com>
2020-07-25 22:15:22 +0300

    First working approach

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+app.get("/", function (req, res) {
+  let id = req.query.id;
+  let rpp = req.query.rpp || 0;
+  let page = req.query.page || 1;
+  let response = `
+    <html>
+        <head>
+            <script src='https://article.omgcheckitout.com/articles/${id}.js'></script>
+        </head>
+        <body>
+            <div id='container'>work</div>
+            <script type='text/javascript'>
+                window.shinez.id = window.shinez.id || '${id}';
+                window.shinez.rpp = window.shinez.rpp || ${rpp};
+                window.shinez.page = window.shinez.page || ${page};
+            </script>
+            <script src='/script.js'></script>
+        </body>
+    </html>
+    `;
+  res.send(response);
+});
+
+app.listen(3000);
---
+++ public/script.js
@@ -0,0 +1,66 @@
+const article = window.shinez.article;
+const id = window.shinez.id;
+const rpp = window.shinez.rpp;
+const page = window.shinez.page;
+
+const blocksToShow = (rpp, page, blocks) => {
+  if (rpp === 0) return blocks;
+  const skip = rpp * (page - 1);
+  const take = rpp;
+  return blocks.slice(skip, skip + take);
+};
+
+const container = document.getElementById("container");
+const createElements = (data) => {
+  data.forEach((el) => {
+    container.appendChild(createElement(el));
+  });
+};
+
+const createElement = (block) => {
+  console.log(block);
+  const title = block.title || "";
+  const texts = block.text;
+  const imageUrl = `https://img.omgcheckitout.com/articles/${id}/${block.image.name}`;
+  const elementContainer = document.createElement("div");
+  elementContainer.innerHTML = `
+        <h3>${title}</h3>
+        <img src='${imageUrl}'>
+        `;
+  elementContainer.appendChild(createTextsBlock(texts));
+  return elementContainer;
+};
+
+const createTextsBlock = (texts) => {
+  const textsContainer = document.createElement("div");
+  for (const text of texts) {
+    const textContainer = document.createElement("div");
+    textContainer.innerHTML = text;
+    textsContainer.appendChild(textContainer);
+  }
+  return textsContainer;
+};
+
+const createPaging = (articleId, rpp, page, total) => {
+  const isNotFirst = page !== 1;
+  const isNotLast = total - rpp * page > 0;
+
+  if (isNotFirst)
+    container.appendChild(pageLink(articleId, " <-- Previous ", rpp, page - 1));
+  if (isNotLast)
+    container.appendChild(pageLink(articleId, " Next --> ", rpp, page + 1));
+};
+
+const pageLink = (articleId, text, rpp, page) => {
+  const linkContainer = document.createElement("a");
+  linkContainer.setAttribute(
+    "href",
+    `/?id=${articleId}&rpp=${rpp}&page=${page}`
+  );
+  linkContainer.innerText = text;
+
+  return linkContainer;
+};
+
+createElements(blocksToShow(rpp, page, article.blocks));
+createPaging(id, rpp, page, article.blocks.length);
